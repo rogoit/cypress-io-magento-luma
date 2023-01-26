5f197878aae7fb9bc68626cb3a7380bc33c9aec4
Lennard Tillmann <lennard@nevercodealone.de>
2023-01-26 16:43:53 +0100

    Moved functions from pageobject into test.

--- cypress/e2e/category.cy.ts
+++ cypress/e2e/category.cy.ts
@@ -9,13 +9,41 @@ describe('Category page tests', () => {
   });
 
   it('Filters for color', () => {
-    categoryPage.setFilterColor();
+    setFilterColor();
     cy.url().should('contain', '?color=');
     expect(selectors.filterValue.toLowerCase).to.contain(selectors.filterColor.toLowerCase);
   });
 
   it('Sorts products from lowest to highest', () => {
-    categoryPage.sortByPrice();
-    categoryPage.validatePrices();
+    sortByPrice();
+    validatePrices();
   });
 });
+
+function setFilterColor() {
+  cy.contains(selectors.filterColor).click();
+  cy.get(selectors.selectColorRed).click();
+}
+
+function sortByPrice() {
+  cy.get(selectors.sorter).select('Preis');
+}
+
+function validatePrices() {
+  //push product prices into array
+  const pricesArray: number[] = [];
+  cy.get(selectors.productPriceID)
+    .children(selectors.prices)
+    .each((price) => {
+      const priceTrimmed = price.text().trim().replace(/,/g, '.').replace(/€/g, '');
+      pricesArray.push(parseFloat(priceTrimmed));
+    })
+    //then validate prices are increasing
+    .then(() => {
+      pricesArray.forEach((price, i) => {
+        if (i + 1 < pricesArray.length) {
+          expect(price).to.be.lte(pricesArray[i + 1]);
+        }
+      });
+    });
+}
--- cypress/pages/CategoryPage.ts
+++ cypress/pages/CategoryPage.ts
@@ -1,32 +1,4 @@
 import { BasePage } from './BasePage';
 import selectors from '../fixtures/selectors/category.json';
 
-export class CategoryPage extends BasePage {
-  setFilterColor() {
-    cy.contains(selectors.filterColor).click();
-    cy.get(selectors.selectColorRed).click();
-  }
-
-  sortByPrice() {
-    cy.get(selectors.sorter).select('Preis');
-  }
-
-  validatePrices() {
-    //push product prices into array
-    const pricesArray: number[] = [];
-    cy.get(selectors.productPriceID)
-      .children(selectors.prices)
-      .each((price) => {
-        const priceTrimmed = price.text().trim().replace(/,/g, '.').replace(/€/g, '');
-        pricesArray.push(parseFloat(priceTrimmed));
-      })
-      //then validate prices are increasing
-      .then(() => {
-        pricesArray.forEach((price, i) => {
-          if (i + 1 < pricesArray.length) {
-            expect(price).to.be.lte(pricesArray[i + 1]);
-          }
-        });
-      });
-  }
-}
+export class CategoryPage extends BasePage {}
