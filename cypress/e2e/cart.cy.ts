b756a874b4e8d60f44752a7cfa0aeedd65e547a8
Lennard Tillmann <lennard@nevercodealone.de>
2023-02-23 16:18:12 +0100

    Extended shopping cart tests.

--- cypress/e2e/cart.cy.ts
+++ cypress/e2e/cart.cy.ts
@@ -1,14 +1,45 @@
 import selectors from '../fixtures/selectors/cart.json';
+import { CartPage } from '@/pages/CartPage';
 
 describe('Shopping cart tests', () => {
   beforeEach(() => {
     cy.visit('/didi-sport-watch.html');
-    cy.get(selectors.buttonAddToCart).click();
-    cy.wait(3000);
-    cy.get(selectors.miniCart).click();
-    cy.get(selectors.buttonViewCart).click();
+    cy.get(selectors.buttonAddToCart).click({ scrollBehavior: 'center' });
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    //cy.wait(3000);
+    cy.get(selectors.notificationMessageTop)
+      .should('be.visible')
+      .should('contain.text', 'Sie haben Didi Sport Watch zum Warenkorb hinzugefügt.');
+    cy.get(selectors.miniCart).should('be.visible').click({ scrollBehavior: 'center' });
+    cy.get(selectors.buttonViewCart).should('be.visible').click({ scrollBehavior: 'center' });
   });
   it('Can add product to cart', () => {
-    cy.get(selectors.itemAmountInCart).should('have.attr', 'value').should('eq', '1');
+    cy.get(selectors.itemAmountInCart).should('be.visible').should('have.attr', 'value').should('eq', '1');
+  });
+
+  it('Can change quantity in card', () => {
+    cy.get(selectors.inputAmountInCard).should('be.visible').clear().type('2{enter}').should('have.value', '2');
+  });
+
+  it('Can remove product from card', () => {
+    CartPage.removeArticle(0);
+    cy.get(selectors.cartEmptyTextField)
+      .should('contain.text', 'Sie haben keine Artikel im Warenkorb')
+      .should('be.visible');
+  });
+
+  it('Can not add an invalid coupon', () => {
+    CartPage.addCouponCode('123abc');
+    cy.get(selectors.notificationMessageTop)
+      .should('be.visible')
+      .should('contain.text', `Der Gutscheincode "123abc" ist ungültig.`);
+  });
+
+  it('Can add a valid coupon', () => {
+    CartPage.addProductToCartByUrl('/affirm-water-bottle.html');
+    CartPage.addCouponCode('H20');
+    cy.get(selectors.notificationMessageTop)
+      .should('be.visible')
+      .should('contain.text', 'Sie haben den Gutscheincode "H20" verwendet.');
   });
 });
--- cypress/pages/CartPage.ts
+++ cypress/pages/CartPage.ts
@@ -1,1 +1,30 @@
 import { BasePage } from './BasePage';
+import selectors from '../fixtures/selectors/cart.json';
+
+export class CartPage extends BasePage {
+  static removeArticle(index) {
+    cy.get(selectors.removeArticle).eq(index).click({ scrollBehavior: 'center' });
+  }
+
+  static addProductToCartByUrl(url) {
+    cy.visit(url);
+    cy.get(selectors.buttonAddToCart).should('be.visible').click({ scrollBehavior: 'center' });
+    cy.get(selectors.notificationMessageTop).should('be.visible').should('contain.text', 'Sie haben');
+    cy.get(selectors.miniCart).should('be.visible').click({ scrollBehavior: 'center' });
+    cy.get(selectors.buttonViewCart).should('be.visible').click({ scrollBehavior: 'center' });
+  }
+
+  static addCouponCode(code) {
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(500);
+    cy.get(selectors.activateCouponField)
+      .should('contain', 'Gutscheincode anwenden')
+      .should('be.visible')
+      .click({ scrollBehavior: 'center' });
+    cy.get(selectors.couponCodeField)
+      .should('not.be.hidden')
+      .should('be.visible')
+      .scrollIntoView()
+      .type(`${code}{enter}`);
+  }
+}
