02d36c4826a569f380cc496b476000932c9e6abd
Lennard Tillmann <lennard@nevercodealone.de>
2023-02-24 13:38:22 +0100

    Removed coupon code tests from shopping cart tests.

--- cypress/e2e/cart.cy.ts
+++ cypress/e2e/cart.cy.ts
@@ -5,14 +5,13 @@ describe('Shopping cart tests', () => {
   beforeEach(() => {
     cy.visit('/didi-sport-watch.html');
     cy.get(selectors.buttonAddToCart).click({ scrollBehavior: 'center' });
-    // eslint-disable-next-line cypress/no-unnecessary-waiting
-    //cy.wait(3000);
     cy.get(selectors.notificationMessageTop)
       .should('be.visible')
       .should('contain.text', 'Sie haben Didi Sport Watch zum Warenkorb hinzugefügt.');
     cy.get(selectors.miniCart).should('be.visible').click({ scrollBehavior: 'center' });
     cy.get(selectors.buttonViewCart).should('be.visible').click({ scrollBehavior: 'center' });
   });
+
   it('Can add product to cart', () => {
     cy.get(selectors.itemAmountInCart).should('be.visible').should('have.attr', 'value').should('eq', '1');
   });
@@ -26,20 +25,5 @@ describe('Shopping cart tests', () => {
     cy.get(selectors.cartEmptyTextField)
       .should('contain.text', 'Sie haben keine Artikel im Warenkorb')
       .should('be.visible');
-  });
-
-  it('Can not add an invalid coupon', () => {
-    CartPage.addCouponCode('123abc');
-    cy.get(selectors.notificationMessageTop)
-      .should('be.visible')
-      .should('contain.text', `Der Gutscheincode "123abc" ist ungültig.`);
-  });
-
-  it('Can add a valid coupon', () => {
-    CartPage.addProductToCartByUrl('/affirm-water-bottle.html');
-    CartPage.addCouponCode('H20');
-    cy.get(selectors.notificationMessageTop)
-      .should('be.visible')
-      .should('contain.text', 'Sie haben den Gutscheincode "H20" verwendet.');
   });
 });
--- cypress/pages/CartPage.ts
+++ cypress/pages/CartPage.ts
@@ -7,24 +7,11 @@ export class CartPage extends BasePage {
   }
 
   static addProductToCartByUrl(url) {
+    cy.log('Add product to cart with param url: ' + url);
     cy.visit(url);
     cy.get(selectors.buttonAddToCart).should('be.visible').click({ scrollBehavior: 'center' });
     cy.get(selectors.notificationMessageTop).should('be.visible').should('contain.text', 'Sie haben');
     cy.get(selectors.miniCart).should('be.visible').click({ scrollBehavior: 'center' });
     cy.get(selectors.buttonViewCart).should('be.visible').click({ scrollBehavior: 'center' });
-  }
-
-  static addCouponCode(code) {
-    // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(500);
-    cy.get(selectors.activateCouponField)
-      .should('contain', 'Gutscheincode anwenden')
-      .should('be.visible')
-      .click({ scrollBehavior: 'center' });
-    cy.get(selectors.couponCodeField)
-      .should('not.be.hidden')
-      .should('be.visible')
-      .scrollIntoView()
-      .type(`${code}{enter}`);
   }
 }
