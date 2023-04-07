c065a2842b04921acf203654d9ca549155554b38
Lennard Tillmann <lennard@nevercodealone.de>
2023-04-07 17:10:22 +0200

    Implemented startpage tests.

---
+++ cypress/e2e/startpage.cy.ts
@@ -0,0 +1,32 @@
+import selectors from '../fixtures/selectors/startpage.json';
+import { StartPage } from '@/pages/StartPage';
+
+describe('Startpage tests', () => {
+  const startPage: StartPage = new StartPage();
+
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('Startpage valid and contains products', () => {
+    cy.get(selectors.welcomeMessage).should('be.visible');
+    cy.contains(selectors.loginButton).should('be.visible');
+    cy.contains(selectors.registerButton).should('be.visible');
+    cy.get('footer').should('be.visible');
+    cy.get(selectors.search).should('be.visible');
+    cy.get(selectors.mainContent).should('be.visible').should('not.be.empty');
+    cy.get(selectors.productItems).should('be.visible').should('have.length.gte', 1);
+  });
+
+  it('Can open category', () => {
+    cy.contains('Women').click();
+    cy.url().should('contain', 'women');
+  });
+
+  it('Validate newsletter', () => {
+    cy.get(selectors.newsletterRegistration).should('be.visible').type('TESTIFY@mail.test {enter}');
+    cy.get(selectors.newsletterConfirmSuccess).should('be.visible');
+    cy.get(selectors.newsletterRegistration).type('.invalidEmail. {enter}');
+    cy.get(selectors.newsletterError).should('be.visible');
+  });
+});
